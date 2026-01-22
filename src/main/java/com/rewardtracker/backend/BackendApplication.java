62dd6c5df156071b8f2cf54f35daac20a22b1952
Iris Cheung <hokyinirischeung@gmail.com>
2026-01-22 00:19:45 -0800

    add content to make sure it display

--- src/main/java/com/rewardtracker/backend/BackendApplication.java
+++ src/main/java/com/rewardtracker/backend/BackendApplication.java
@@ -1,13 +1,34 @@
+// package com.rewardtracker.backend;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class BackendApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(BackendApplication.class, args);
+// 	}
+
+// }
+
 package com.rewardtracker.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
+	@RequestMapping("/")  // ← 新增 Hello endpoint
+    public String home() {
+        return "Hello Docker World - Reward Tracker Backend";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
+
