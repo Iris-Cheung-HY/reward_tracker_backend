34160d252e2fd1ac5e9fc13db5a82a0406943b54
Iris Cheung <hokyinirischeung@gmail.com>
2026-01-22 23:09:24 -0800

    revise application properties file

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM eclipse-temurin:25-jdk-alpine AS builder
+# Build jar
+FROM eclipse-temurin:25-jdk-alpine AS builder 
 WORKDIR /app
 COPY . .
 RUN ./mvnw clean package -DskipTests
 
+
+#Use jar to run service
 FROM eclipse-temurin:25-jre-alpine
 WORKDIR /app
 COPY --from=builder /app/target/backend-0.0.1-SNAPSHOT.jar app.jar
--- src/main/java/com/rewardtracker/backend/BackendApplication.java
+++ src/main/java/com/rewardtracker/backend/BackendApplication.java
@@ -1,17 +1,3 @@
-// package com.rewardtracker.backend;
-
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class BackendApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(BackendApplication.class, args);
-// 	}
-
-// }
-
 package com.rewardtracker.backend;
 
 import org.springframework.boot.SpringApplication;
@@ -22,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class BackendApplication {
-	@RequestMapping("/")  // ← 新增 Hello endpoint
+	@RequestMapping("/")  
     public String home() {
         return "Hello Docker World - Reward Tracker Backend";
     }
