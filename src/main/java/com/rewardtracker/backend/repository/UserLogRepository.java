09572ae20652ad8dff63f6ce070ca1bc80ca799e
Iris Cheung <hokyinirischeung@gmail.com>
2026-01-27 18:58:24 -0800

    add dependencies for UserLogRepository:

--- src/main/java/com/rewardtracker/backend/repository/UserLogRepository.java
+++ src/main/java/com/rewardtracker/backend/repository/UserLogRepository.java
@@ -4,6 +4,7 @@ import com.rewardtracker.backend.model.UserLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
 public interface UserLogRepository extends JpaRepository <UserLog, Long> {
