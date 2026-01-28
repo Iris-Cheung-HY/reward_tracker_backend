9bf557548c26c4506e1d47ee1dbb06e7e90c05bb
Iris Cheung <hokyinirischeung@gmail.com>
2026-01-27 16:28:54 -0800

    added user method to userlogrepo

--- src/main/java/com/rewardtracker/backend/repository/UserLogRepository.java
+++ src/main/java/com/rewardtracker/backend/repository/UserLogRepository.java
@@ -4,7 +4,6 @@ import com.rewardtracker.backend.model.UserLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserLogRepository extends JpaRepository <UserLog, Long> {
